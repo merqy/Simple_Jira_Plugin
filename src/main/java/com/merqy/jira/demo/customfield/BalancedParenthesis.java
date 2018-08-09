92e7c5149208f424dadc4e6553c869228968353f
Merqy <pgolubev7@gmail.com>
2018-08-09 20:47:39 +0300

    Deleted commented part of useless code

--- src/main/java/com/merqy/jira/demo/customfield/BalancedParenthesis.java
+++ src/main/java/com/merqy/jira/demo/customfield/BalancedParenthesis.java
@@ -1,44 +1,4 @@
-package com.merqy.jira.demo.customfield;/*
 package com.merqy.jira.demo.customfield;
-
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.customfields.impl.TextCFType;
-import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
-import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
-import com.atlassian.jira.issue.fields.CustomField;
-import com.atlassian.jira.issue.fields.config.FieldConfig;
-import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-
-public class BalancedParenthesis extends TextCFType {
-    private static final Logger log = LoggerFactory.getLogger(BalancedParenthesis.class);
-
-    public BalancedParenthesis(CustomFieldValuePersister customFieldValuePersister, GenericConfigManager genericConfigManager) {
-    super(customFieldValuePersister, genericConfigManager);
-}
-    
-    @Override
-    public Map<String, Object> getVelocityParameters(final Issue issue,
-                                                     final CustomField field,
-                                                     final FieldLayoutItem fieldLayoutItem) {
-        final Map<String, Object> map = super.getVelocityParameters(issue, field, fieldLayoutItem);
-
-        // This method is also called to get the default value, in
-        // which case issue is null so we can't use it to add currencyLocale
-        if (issue == null) {
-            return map;
-        }
-
-         FieldConfig fieldConfig = field.getRelevantConfig(issue);
-         //add what you need to the map here
-
-        return map;
-    }
-}
-*/
 
 import com.atlassian.jira.issue.customfields.impl.AbstractSingleFieldType;
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
